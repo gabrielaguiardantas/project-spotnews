020ff47888e881e087fd9f533709d5c87d056b61
gabrielaguiardantas <gabrielaguiardantas@gmail.com>
2023-09-07 22:23:24 -0300

    página home e details criada, req. 4, 5

--- news/views.py
+++ news/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from news.models.news_model import News
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+
+def home(request):
+    context = {"news": News.objects.all()}
+    return render(request, "home.html", context)
+
+
+def news_details(request, news_id):
+    news = get_object_or_404(News, id=news_id)
+    return render(
+        request,
+        "news_details.html",
+        {"news": news, "categories": news.categories.all()},
+    )
